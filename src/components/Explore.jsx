705388da51af1c9765a64b3ea998208bcc1273af
darakhsha rayen <darakhsharayen9@gmail.com>
2024-07-09 11:37:59 +0530

    done

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import Footer from './Footer';
+import './Explore.css'; // Make sure to create and use an appropriate CSS file
+import searchIcon from '../logo/search-interface-symbol.png'; // Ensure the path is correct
 
 const Explore = () => {
   return (
-    <div>Explore</div>
-  )
-}
+    <>
+    <div className='explore'>Explore</div>
+      <div className="explore-container">
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." aria-label="Search" style={{ backgroundImage: `url(${searchIcon})` }} />
+          
+        </div>
+        <div className="category-buttons">
+          <button>Quests (695)</button>
+          <button>NFTs (116)</button>
+          <button>Communities (671)</button>
+          <button>Events (31)</button>
+          <button>External Quests (12855)</button>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default Explore
+export default Explore;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Navbar.css';
 import { Link } from 'react-router-dom';
 import logo from '../logo/intract_text.svg';
 
 const Navbar = () => {
+    const [isCompassHovered, setIsCompassHovered] = useState(false);
+
     return (
         <nav className="navbar">
             <div className="navbar-brand">
                 <img src={logo} alt="intract logo" />
             </div>
             <ul className="navbar-nav">
-                <li className="nav-item"><Link to="/compass">Compass</Link></li>
+                <li 
+                    className="nav-item"
+                    onMouseEnter={() => setIsCompassHovered(true)}
+                    onMouseLeave={() => setIsCompassHovered(false)}
+                >
+                    <Link to="/compass">Compass</Link>
+                    {isCompassHovered && (
+                        <div className="popup">
+                            <div>All Ecosystems</div>
+                        </div>
+                    )}
+                </li>
                 <li className="nav-item"><Link to="/explore">Explore</Link></li>
                 <li className="nav-item"><Link to="/">Academy</Link></li>
                 <li className="nav-item"><Link to="/nfts">NFTs</Link></li>
